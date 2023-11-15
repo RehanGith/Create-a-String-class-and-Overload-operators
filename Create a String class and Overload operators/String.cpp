206b57ea5f0b4f9d9f61f7f4cc7e331238314ae7
RehanGith <chrehan1007@gmail.com>
2023-11-15 11:59:41 +0500

    Create basis constructor and Destructor

--- Create a String class and Overload operators/String.cpp
+++ Create a String class and Overload operators/String.cpp
@@ -1,1 +1,32 @@
 #include "String.h"
+#include <cstring>
+#include <iostream>
+String::String()
+	: str(nullptr) {
+	str = new char;
+	*str = '\0';
+}
+String::String(char* s)
+	: str(nullptr) {
+	if (s == nullptr) {
+		str = new char;
+		*str = '\0';
+	}
+	else {
+		size_t size = strlen(s) + 1;
+		str = new char[size];
+		strcpy_s(str , size, s);
+	}
+}
+String::String(const String& source)
+: str(nullptr) {
+	size_t size = std::strlen(source.str) + 1;
+	str = new char[size];
+	strcpy_s(str, size, source.str);
+}
+void String::display() const {
+	std::cout << str << std::endl;
+}
+String::~String() {
+	delete[] str;
+}
--- Create a String class and Overload operators/main.cpp
+++ Create a String class and Overload operators/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "String.h"
 using namespace std;
 
 int main() {
-
+	String s;
+	char arr[]{ "Rehan" };
+	String s1(arr);
+	s1.display();
+	s.display();
+	String s2(s1);
+	s2.display();
 	return 0;
 }
